0b367402278c0b3a0d189bc6d024327416cb1fe7
Shorya Rawat <rawatshorya@gmail.com>
2022-06-11 11:57:13 +0530

    📝

---
+++ Apna College/Arrays/circular_subarraysum.cpp
@@ -0,0 +1,41 @@
+#include<iostream>
+#include<climits>
+using namespace std;
+
+int kdane(int arr[], int n)
+{
+    int currSum = 0;
+    int maxSum = INT_MIN;
+
+    for(int i =0;i<n;i++)
+    {
+        currSum+=arr[i];
+        if(currSum<0){
+            currSum = 0;
+        }
+        maxSum = max(maxSum, currSum);
+    }
+
+    return maxSum;
+}
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n; i++)
+    {
+        cin>>arr[i];
+    }
+
+    int unwrap = kdane(arr, n); //kadane algo
+    int total = 0; 
+    for(int i =0; i<n; i++)
+    {
+        total += arr[i];
+        arr[i] = -arr[i];
+    }
+    int wrap = total + kdane(arr, n);
+    cout<<max(wrap, unwrap);
+}
