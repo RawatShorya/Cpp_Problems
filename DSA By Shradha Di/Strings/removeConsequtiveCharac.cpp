bc3891295c522f14e9dabdfe2ca050c115e0592b
Shorya Rawat <rawatshorya@gmail.com>
2022-07-23 17:41:51 +0530

    🐛

--- DSA By Shradha Di/Strings/removeConsequtiveCharac.cpp
+++ DSA By Shradha Di/Strings/removeConsequtiveCharac.cpp
@@ -1,25 +1,25 @@
  
  class Solutions{
- public:
-    string removeConsecutiveCharacter(string s)
-    {
-        // code here.
-        for(int i = 1;i<s.size();i++)
-        {
-            if(s[i] == s[i-1])
-            s[i-1] = ' ';
-        }
-        
-        int space =0;
-        for(int i=0;i<s.size();i++)
-        {
-            if(s[i] != ' ')
-            {
-                swap(s[i], s[space]);
-                space++;
-            }
-        }
-        return s;
-        
-    }
+    public:
+       string removeConsecutiveCharacter(string s)
+       {
+           // code here.
+           for(int i = 1;i<s.size();i++)
+           {
+               if(s[i] == s[i-1])
+               s[i-1] = ' ';
+           }
+           
+           int space =0;
+           for(int i=0;i<s.size();i++)
+           {
+               if(s[i] != ' ')
+               {
+                   swap(s[i], s[space]);
+                   space++;
+               }
+           }
+           return s;
+           
+       }
 };
